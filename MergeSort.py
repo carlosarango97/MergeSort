26136f182acb041097aa2f1e6d16c975a29395e1
Carlos Arango <47753930+carlosarango97@users.noreply.github.com>
2021-10-18 22:10:03 -0500

    Merge sort file was created

---
+++ MergeSort.py
@@ -0,0 +1,9 @@
+def merge_sort(myList):
+    #Put your code here
+
+lista_ejemplo = [58, 52, 5, 12, 69, 87, 12]
+merge_sort(lista_ejemplo)
+
+print ("Lista ordenada:")
+for i in lista_ejemplo:
+	print (str(i))
